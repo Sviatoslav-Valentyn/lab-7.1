1319d88649591a866b0cba16f5b8929619391a2b
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2021-11-16 16:24:16 +0200

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 7.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int** R = new int* [2];
+			for (int i = 0; i < 2;)R[i++] = new int[2];
+			R[0][0] = 2;
+			R[0][0] = 0;
+			R[0][0] = -1;
+			R[1][1] = 1;
+
+			Change(R, 0, 1, 2);
+			Assert::AreEqual(1, R[0][1]);
+		}
+	};
+}
